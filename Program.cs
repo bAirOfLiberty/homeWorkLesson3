21de7f8df56e526c0ace617797eb452f33da2ee4
bair <bair.syngeev@yandex.ru>
2023-11-29 21:31:58 +0300

    done third

--- Program.cs
+++ Program.cs
@@ -13,14 +13,38 @@
 // Console.WriteLine(result);
 
 // Задача 2: Задайте массив из 10 целых чисел. Напишите программу, которая определяет количество четных чисел в массиве.
-Console.Clear();
-int[] array = {19,27,34,42,53,66,72,81,90,1012};
-int result = 0;
-for(int i=0; i<10; i++)
-{
-    if(array[i] % 2 == 0)
-    {
-        result++;
-    }
-}
-Console.WriteLine(result);
+// Console.Clear();
+// int[] array = {19,27,34,42,53,66,72,81,90,1012};
+// int result = 0;
+// for(int i=0; i<10; i++)
+// {
+//     if(array[i] % 2 == 0)
+//     {
+//         result++;
+//     }
+// }
+// Console.WriteLine(result);
+
+// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. 
+// Найдите разницу между максимальной и минимальным элементом массива.
+// Console.Clear();
+// double[] array = { 12312.3321, 1331.2134, 16123.3312, 1999.836, 8888.09231 };
+// int count = array.Length;
+// double min = array[0];
+// for (int i = 1; i < count; i++)
+// {
+//     if (min > array[i])
+//     {
+//         min = array[i];
+//     }
+// }
+//  double max = array [0];
+//  for (int i = 1; i < count; i++)
+// {
+//     if (max <array[i])
+//     {
+//         max = array[i];
+//     }
+
+// }
+// Console.WriteLine(max - min);
